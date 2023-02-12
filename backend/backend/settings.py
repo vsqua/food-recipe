c1a5678540d6347722c52ee3e92b0f65e9d79e9c
Anandan MB <anandanmb01@gmail.com>
2023-02-12 07:12:14 +0530

    added env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+
+#env setup
+from dotenv import load_dotenv
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
